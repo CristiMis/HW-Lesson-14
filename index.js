c972fb28d437509319667eba3ec62771764738bf
CristiMis <mistreanu10@outlook.com>
2024-03-11 14:20:11 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+// function SumAndRadical(a, b, c) {
+
+//   const sum = a + b + c 
+//   if (sum >= 0) {
+//     const radical = Math.sqrt(sum)
+
+//     return radical
+//   } else {
+//     return "Error"
+//   }
+// }
+// console.log(SumAndRadical(1, 2, 3)) 
+// console.log(SumAndRadical(5, 10, 15))  
+// console.log(SumAndRadical(-1, -2, -3))
+
+
+
+// function RandomLetter(str) {
+  
+//   const randomIndex = Math.floor(Math.random() * str.length)
+//   return str[randomIndex]
+// }
+// const str = 'text'
+// const randomLetter = RandomLetter(str)
+// console.log(randomLetter)
+
+
+
+const str1 = 'Bună'
+const str2 = 'Ziua'
+let concatinate = str1.concat(str2)
+console.log(concatinate)
